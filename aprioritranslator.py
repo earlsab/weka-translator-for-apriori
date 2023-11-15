bb41da72e75912f8b8ae8cf58bdbbf035f996561
earlsab <62688314+earlsab@users.noreply.github.com>
2023-11-15 16:22:02 +0800

    remove id & polished code

--- aprioritranslator.py
+++ aprioritranslator.py
@@ -7,15 +7,23 @@ import csv
 # milk,bread,?,?
 
 # Into
-# | id | Milk | Bread |...
-# 1, TRUE, FALSE
+# | Milk | Bread |...
+# TRUE, TRUE, ?,?
 
+# CONFIGURATION
 unique_items = []
 ignore_items = [' ','', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+true_value = 't'
+false_value = '?'
+input_file = 'basket.csv'
+output_file = 'output.csv'
+
+
+#####
 
 row_count = 0
 column_count = 0
-with open('basket.csv') as csv_file_reader:
+with open(input_file) as csv_file_reader:
     csv_reader = csv.reader(csv_file_reader, delimiter=',')
 
     # Read Unique Items
@@ -26,8 +34,8 @@ with open('basket.csv') as csv_file_reader:
             if (not item in unique_items) and (not item in ignore_items):
                 unique_items.append(item)
 
-with open('basket.csv') as csv_file_reader:
-    with open('output.csv', 'w', newline='') as csv_file_writer:
+with open(input_file) as csv_file_reader:
+    with open(output_file, 'w', newline='') as csv_file_writer:
         # Headers
         csv_reader2 = csv.reader(csv_file_reader, delimiter=',')
         csv_writer = csv.writer(csv_file_writer, lineterminator='\n')
@@ -36,20 +44,15 @@ with open('basket.csv') as csv_file_reader:
         for row in csv_reader2:
             row_staging = []
             row_count += 1
-            # row_staging.append(row_count)
-
 
             for index in range(0, len(unique_items)):
                 if row_count != 1:  # Ignore first row
                     if unique_items[index] in row:
-                        row_staging.append("TRUE") 
+                        row_staging.append(true_value) 
                     else:
-                        row_staging.append("FALSE") 
+                        row_staging.append(false_value) 
        
             csv_writer.writerow(row_staging)
-
-
-
 
 
 print(unique_items)
